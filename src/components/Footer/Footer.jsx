7b342d3c1a5afd368ed0094c99e4c6e29fc93d95
Nabraj Poudel <nabrajpoudel.28@gmail.com>
2025-02-28 11:12:59 +0545

    UPDATE: Updated Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,38 +1,32 @@
-import styles from './footer.module.css'
+import styles from './footer.module.css';
+import { MdOutlinePhoneInTalk, MdLocationPin, MdOutlineMail } from "react-icons/md";
 
 const Footer = () => {
+
+    const contactDetails = [
+        { Icon: MdLocationPin, title: "Find Us", content: "Pokhara, Nepal" },
+        { Icon: MdOutlinePhoneInTalk, title: "Call Us", content: "+977 9867795254" },
+        { Icon: MdOutlineMail, title: "Mail Us", content: "nabrajpoudel.28@gmail.com" }
+    ];
+
     return (
         <footer>
             <div className={styles.contactContainer}>
                 <div className={styles.contactContent}>
-                    <div>
-
-                        <div>
-                            <h1>Find Us</h1>
-                            <p>Pokhara, Nepal</p>
+                    {contactDetails.map((detail, index) => (
+                        <div key={index} className={styles.contentBox}>
+                            <detail.Icon className={styles.icon} />
+                            <div>
+                                <h1>{detail.title}</h1>
+                                <p>{detail.content}</p>
+                            </div>
                         </div>
-                    </div>
-
-                    <div>
-
-                        <div>
-                            <h1>Call Us</h1>
-                            <p>+977 9867795254</p>
-                        </div>
-                    </div>
-
-                    <div>
-
-                        <div>
-                            <h1>Mail Us</h1>
-                            <p>nabrajpoudel.28@gmail.com</p>
-                        </div>
-                    </div>
+                    ))}
                 </div>
             </div>
             <div className={styles.copyRightContainer}>
                 <p className={styles.copyRightContent}>
-                    Copyright © 2024, All Right Reserved <span>Nab Raj Poudel</span>
+                    Copyright © {new Date().getFullYear()}, All Right Reserved <span>Nab Raj Poudel</span>
                 </p>
             </div>
         </footer>
