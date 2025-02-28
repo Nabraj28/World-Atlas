54b271d112a545595b9598cb2e6f360736a7ccf7
Nabraj Poudel <nabrajpoudel.28@gmail.com>
2025-02-28 09:47:58 +0545

    UPDATE: Updated footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,21 +4,36 @@ const Footer = () => {
     return (
         <footer>
             <div className={styles.contactContainer}>
-                <div>
-                    <h1>Find Us</h1>
-                    <p>Pokhara, Nepal</p>
-                </div>
-                <div>
-                    <h1>Call Us</h1>
-                    <p>+977 9867795254</p>
-                </div>
-                <div>
-                    <h1>Mail Us</h1>
-                    <p>nabrajpoudel.28@gmail.com</p>
+                <div className={styles.contactContent}>
+                    <div>
+
+                        <div>
+                            <h1>Find Us</h1>
+                            <p>Pokhara, Nepal</p>
+                        </div>
+                    </div>
+
+                    <div>
+
+                        <div>
+                            <h1>Call Us</h1>
+                            <p>+977 9867795254</p>
+                        </div>
+                    </div>
+
+                    <div>
+
+                        <div>
+                            <h1>Mail Us</h1>
+                            <p>nabrajpoudel.28@gmail.com</p>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className={styles.copyrightContainer}>
-                <p>Copyright © 2024, All Right ReservedNab Raj Poudel</p>
+            <div className={styles.copyRightContainer}>
+                <p className={styles.copyRightContent}>
+                    Copyright © 2024, All Right Reserved <span>Nab Raj Poudel</span>
+                </p>
             </div>
         </footer>
     )
